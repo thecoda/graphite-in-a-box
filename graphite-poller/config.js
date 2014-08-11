c0b46181f4fcc90f2e74ba62297e80876940789b
Kevin Wright <kevin1.wright@hsbc.com>
2014-08-11 16:09:49 +0100

    Removed poller's config.js and replaced with config.sample.js, config.js is now git-ignored.

--- graphite-poller/config.js
+++
@@ -1,8 +0,0 @@
-{
-  "poll_interval": 10,
-  "hosts": [
-    { "name": "dev.example.com", "port": 9500 },
-    { "name": "stage.example.com", "port": 9500 },
-    { "name": "prod.example.com", "port": 9500 }
-  ]
-}
---
+++ graphite-poller/config.sample.js
@@ -0,0 +1,8 @@
+{
+  "poll_interval": 10,
+  "hosts": [
+    { "name": "dev.example.com", "port": 8080, "prefix": "dev" },
+    { "name": "stage.example.com", "port": 8080, "prefix": "stage" },
+    { "name": "prod.example.com", "port": 80, "prefix": "prod" }
+  ]
+}
